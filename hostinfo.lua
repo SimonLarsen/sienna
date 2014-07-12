1c0e8cddc0145496cdcbecf6002c4b9dbb647a80
Mika Attila <radiantstatue@gmail.com>
2014-07-12 19:59:15 +0200

    hostinfo: Rename 'mod' to 'api'

--- hostinfo.lua
+++ hostinfo.lua
@@ -1,9 +1,9 @@
-local mod = {}
+local api = {}
 
 local os_string = love.system.getOS()
 
-function mod.isTouchDevice()
+function api.isTouchDevice()
 	return os_string == 'Android' or os_string == 'iOS'
 end
 
-return mod
+return api
